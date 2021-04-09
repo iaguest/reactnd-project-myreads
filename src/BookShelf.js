04868cc02fb58ad3a1b9294644965f96a3e58258
ianguest <iag.1.apps@gmail.com>
2021-04-09 20:10:04 +0100

    feat: Complete BookShelf component implementation and usage

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,14 +4,12 @@ import Book from './Book'
 
 function BookShelf(props) {
   return (
-    <div>
+    <div className="bookshelf">
       <h2 className="bookshelf-title">{props.category}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
             { props.books.map((book) => {
-              return <Book book={book} /> })}
-          </li>
+              return <li><Book book={book} /></li> })}         
         </ol>
       </div>
     </div>
